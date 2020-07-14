e26a17b6d4ac02f8c54f735440378caf9735f469
Msoliman0512 <msoliman@silverkeytech.com>
2020-07-14 16:54:05 +0200

    Add hide Date Button

--- JSdemo-functions.js
+++ JSdemo-functions.js
@@ -8,5 +8,21 @@ function message(msg) {
   document.getElementById("output").innerHTML = msg + " event";
 }
 function displayDate() {
+  // initialize the p tag
   document.getElementById("output").innerHTML = Date();
+  // find the element
+  x = document.getElementById("output");
+  //x.style.display = "block";
+
+  //option 2: change the class
+  x.className = "show"; // more convenient
+}
+
+function hideDate() {
+  // find the element
+  x = document.getElementById("output");
+  //x.style.display = "none";
+
+  //option 2: change the class
+  x.className = "hide"; // more convenient
 }
